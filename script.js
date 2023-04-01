9452d43e1bad47decb16c49902fec50b045d0508
Luaniki <jorgeluis04avellacorrea@gmail.com>
2023-04-01 11:54:12 -0500

    fin sesión

--- script.js
+++ script.js
@@ -2,16 +2,47 @@ function countChars(obj){
     document.getElementById("charNum").innerHTML = `Caracteres escritos: ${obj.value.length}`;
 }
 
+function tipoDe(){
+    var value = parseInt(document.getElementById("obj"));
+    document.getElementById("dataType").innerHTML = `Tipo de dato: ${typeof(value)}`;
+}
+
 function mayEdad(edad, nombre){
     var edad = document.getElementById("edad").value;
     var nombre = document.getElementById("nombre").value;
 
     if(edad >=18) {
-        document.getElementById("age").innerHTML = `Hola ${nombre.value}, eres mayor de edad.`;
+        document.getElementById("age").innerHTML = `Hola ${nombre}, eres mayor de edad.`;
     } else if(edad <=17) {
-        document.getElementById("age").innerHTML = `Hola ${nombre.value}, no eres mayor de edad.`;
+        document.getElementById("age").innerHTML = `Hola ${nombre}, no eres mayor de edad.`;
     } else if(edad <=0) {
         document.getElementById("age").innerHTML = `No es una edad válida.`;
+    }
+}
+
+function grades(){
+    var nombr = document.getElementById("nombr").value;
+    var materia = document.getElementById("materia").value;
+    var nota1 = parseFloat(document.getElementById("nota1").value);
+    var nota2 = parseFloat(document.getElementById("nota2").value);
+    var nota3 = parseFloat(document.getElementById("nota3").value);
+
+    var notafinal = (nota1 + nota2 + nota3)/3;
+
+    if(notafinal >=3.0) {
+        document.getElementById("grade").innerHTML = `Felicitaciones ${nombr}, su nota es ${notafinal}. Pasaste la materia ${materia}`;
+    } else if(notafinal <3.0) {
+        document.getElementById("grade").innerHTML = `Lo siento ${nombr}, su nota es ${notafinal}. No pasaste la materia ${materia}`;
+    }
+}
+
+function numPar(){
+    var num = document.getElementById("num").value;
+
+    if(num%2 == 0) {
+        document.getElementById("numEven").innerHTML = `El número ${num} es par`;
+    } else {
+        document.getElementById("numEven").innerHTML = `El número ${num} no es par`;
     }
 }
 
@@ -20,8 +51,8 @@ function divisible(){
     var num2 = document.getElementById("num2").value;
 
     if(num1%num2 == 0) {
-        document.getElementById("modDiv").innerHTML = `El número ${num1.value} es divisible entre el número ${num2.value}`;
+        document.getElementById("modDiv").innerHTML = `El número ${num1} es divisible entre el número ${num2}`;
     } else {
-        document.getElementById("modDiv").innerHTML = `El número ${num1.value} no es divisible entre el número ${num2.value}`;
+        document.getElementById("modDiv").innerHTML = `El número ${num1} no es divisible entre el número ${num2}`;
     }
 }
