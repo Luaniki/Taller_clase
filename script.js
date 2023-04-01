45ef4dd86a7331a385d27ea6087cb927645eb277
Luaniki <jorgeluis04avellacorrea@gmail.com>
2023-03-31 19:28:45 -0500

    css y segundo ejercicio incompleto

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 function countChars(obj){
     document.getElementById("charNum").innerHTML = `Caracteres escritos: ${obj.value.length}`;
 }
+
+function mayEdad(edad, nombre){
+    var edad = document.getElementById("user_age").value;
+    var nombre = document.getElementById("user_name").value;
+
+    if(edad >=17) {
+        document.getElementById("age").innerHTML = `Hola ${nombre.value}, no eres mayor de edad.`;
+    } else if(edad <=18) {
+        document.getElementById("age").innerHTML = `Hola ${nombre.value}, eres mayor de edad.`;
+    } else if(edad >=0) {
+        document.getElementById("age").innerHTML = `No es una edad válida.`;
+    }
+}
